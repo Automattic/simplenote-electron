3855c2b0d047b25c41b9982901461164773232c8
Dan Roundhill <dan@automattic.com>
2015-10-16 10:47:50 -0600

    Load index.html.
    Reviewed in person by @rhc3

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/build/app.html');
+  mainWindow.loadUrl('file://' + __dirname + '/build/index.html');
 
   // Open the devtools.
   mainWindow.openDevTools();
