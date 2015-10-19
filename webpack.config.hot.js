939ef3e7c98cd1916df004caf5ed69c4aff9045f
Dennis Snell <dennis.snell@automattic.com>
2015-10-19 10:42:56 -0600

    Add hotloading webpack server
    ```bash
    webpack-dev-server --content-base dist/ --port 4000 --hot --inline
    --progress --config webpack.config.hot.js
    ```

---
+++ webpack.config.hot.js
@@ -0,0 +1,25 @@
+require('babel/register');
+var webpack = require( 'webpack' );
+var baseConfig = require( './webpack.config.js' );
+
+module.exports = Object.assign( baseConfig, {
+	entry: [
+		'webpack-dev-server/client?http://localhost:4000/dist/',
+		'webpack/hot/only-dev-server',
+		'./app.js'
+	],
+	module: {
+      loaders: [
+	      { test: /\.jsx?$/, exclude: /node_modules/, loaders: [ 'react-hot', 'babel-loader' ] },
+		  { test: /\.json$/, loader: 'json-loader'},
+		  { test: /\.scss$/, loader: 'style!css!sass'}
+      ]
+	},
+	output: Object.assign( baseConfig.output, {
+		publicPath: 'http://localhost:4000/'
+	} ),
+	plugins: baseConfig.plugins.concat( [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	] )
+} );
