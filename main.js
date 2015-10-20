c2c5710e8ff1dda79a4b5533597d9e933d9311c3
Dan Roundhill <dan@automattic.com>
2015-10-20 13:09:30 -0700

    Adds menu items (copied from OS X app).

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var app = require('app');	 // Module to control application life.
+var Menu = require('menu');
 var BrowserWindow = require('browser-window');	// Module to create native browser window.
 
 module.exports = function main(url) {
@@ -21,6 +22,11 @@ module.exports = function main(url) {
 	// This method will be called when Electron has finished
 	// initialization and is ready to create browser windows.
 	app.on('ready', function() {
+		// Configure and set the application menu
+		var menuTemplate = createMenuTemplate();
+		var menu = Menu.buildFromTemplate( menuTemplate );
+		Menu.setApplicationMenu( menu );
+
 		// Create the browser window.
 		mainWindow = new BrowserWindow({width: 800, height: 600});
 
@@ -39,3 +45,163 @@ module.exports = function main(url) {
 		});
 	});
 };
+
+function createMenuTemplate() {
+	var menuTemplate = [ {
+		label: 'Edit',
+		submenu: [ {
+			label: 'Undo',
+			accelerator: 'CmdOrCtrl+Z',
+			role: 'undo'
+		}, {
+			label: 'Redo',
+			accelerator: 'Shift+CmdOrCtrl+Z',
+			role: 'redo'
+		}, {
+			type: 'separator'
+		}, {
+			label: 'Cut',
+			accelerator: 'CmdOrCtrl+X',
+			role: 'cut'
+		}, {
+			label: 'Copy',
+			accelerator: 'CmdOrCtrl+C',
+			role: 'copy'
+		}, {
+			label: 'Paste',
+			accelerator: 'CmdOrCtrl+V',
+			role: 'paste'
+		}, {
+			label: 'Select All',
+			accelerator: 'CmdOrCtrl+A',
+			role: 'selectall'
+		} ]
+	}, {
+		label: 'View',
+		submenu: [ {
+			label: 'Font Size',
+			submenu: [ {
+				label: 'Larger',
+				accelerator: 'CommandOrControl+=', // doh: https://github.com/atom/electron/issues/1507
+				click: function(item, focusedWindow) {
+					debugger;
+					// TODO increase size of editor font-size
+				}
+			}, {
+				label: 'Smaller',
+				accelerator: 'CmdOrCtrl+-'
+			}, {
+				label: 'Reset',
+				accelerator: 'CmdOrCtrl+0'
+			} ]
+		}, {
+			label: 'Sort Order',
+			submenu: [ {
+				label: 'Date Updated'
+			}, {
+				label: 'Alphabetical'
+			} ]
+		}, {
+			label: 'Theme',
+			submenu: [ {
+				label: 'Light'
+			}, {
+				label: 'Dark'
+			} ]
+		}, {
+			label: 'Toggle Full Screen',
+			accelerator: (function() {
+				if (process.platform == 'darwin')
+					return 'Ctrl+Command+F';
+				else
+					return 'F11';
+			})(),
+			click(item, focusedWindow) {
+				if (focusedWindow) {
+					focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+				}
+			}
+		}, {
+			label: 'Toggle Developer Tools',
+			accelerator: (function() {
+				if (process.platform == 'darwin')
+					return 'Alt+Command+I';
+				else
+					return 'Ctrl+Shift+I';
+			})(),
+			click(item, focusedWindow) {
+				if (focusedWindow) {
+					focusedWindow.toggleDevTools();
+				}
+			}
+		} ]
+	}, {
+		label: 'Window',
+		role: 'window',
+		submenu: [ {
+			label: 'Minimize',
+			accelerator: 'CmdOrCtrl+M',
+			role: 'minimize'
+		}, {
+			label: 'Close',
+			accelerator: 'CmdOrCtrl+W',
+			role: 'close'
+		} ]
+	}, {
+		label: 'Help',
+		role: 'help',
+		submenu: [ {
+			label: 'Learn More',
+			click: function() {
+				require('shell').openExternal('http://simplenote.com/help')
+			}
+		} ]
+	} ];
+
+	if (process.platform === 'darwin') {
+		var name = require('app').getName();
+
+		menuTemplate.unshift( {
+			label: name,
+			submenu: [ {
+				label: 'About ' + name,
+				role: 'about'
+			}, {
+				type: 'separator'
+			}, {
+				label: 'Services',
+				role: 'services',
+				submenu: []
+			}, {
+				type: 'separator'
+			}, {
+				label: 'Hide ' + name,
+				accelerator: 'Command+H',
+				role: 'hide'
+			}, {
+				label: 'Hide Others',
+				accelerator: 'Command+Shift+H',
+				role: 'hideothers'
+			}, {
+				label: 'Show All',
+				role: 'unhide'
+			}, {
+				type: 'separator'
+			}, {
+				label: 'Quit',
+				accelerator: 'Command+Q',
+				click: function() { app.quit(); }
+			} ]
+		});
+
+		// Window menu.
+		menuTemplate[3].submenu.push( {
+			type: 'separator'
+		}, {
+			label: 'Bring All to Front',
+			role: 'front'
+		} );
+	}
+
+	return menuTemplate;
+}
