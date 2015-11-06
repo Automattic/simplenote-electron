0bd13570bc1ab7aa923a10707ed6fb7ccdea1493
Serge Paquet <spaquet@cortex.bz>
2015-11-06 14:05:21 -0500

    Use custom dev server for improved hot module loading

--- webpack.config.hot.js
+++ webpack.config.hot.js
@@ -4,12 +4,8 @@ var baseConfig = require( './webpack.config.js' );
 module.exports = Object.assign( {}, baseConfig, {
 	devtool: 'eval-source-map',
 	entry: [
-		'webpack-dev-server/client?http://localhost:4000/',
-		'webpack/hot/only-dev-server'
+		'webpack-hot-middleware/client'
 	].concat(baseConfig.entry),
-	output: Object.assign( {}, baseConfig.output, {
-		publicPath: 'http://localhost:4000/'
-	} ),
 	plugins: baseConfig.plugins.concat( [
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoErrorsPlugin()
