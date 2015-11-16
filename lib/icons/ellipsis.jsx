dfb79122b6cff1efe2107bea2bec3034f28d993a
Serge Paquet <spaquet@cortex.bz>
2015-11-16 12:02:15 -0500

    Indentation fixes

--- lib/icons/ellipsis.jsx
+++ lib/icons/ellipsis.jsx
@@ -6,10 +6,10 @@ export default React.createClass( {
 
 	render: function() {
 		return (
-		<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
-		    <path fill="none" d="M0 0h22v22H0z"/>
-		    <path d="M9 11a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9 2a2 2 0 1 0 0-4 2 2 0 0 0 0 4zM4 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
-		</svg>
+			<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
+				<path fill="none" d="M0 0h22v22H0z"/>
+				<path d="M9 11a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9 2a2 2 0 1 0 0-4 2 2 0 0 0 0 4zM4 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
+			</svg>
 		);
 	}
 } );
--- lib/icons/revisions.jsx
+++ lib/icons/revisions.jsx
@@ -6,10 +6,10 @@ export default React.createClass( {
 
 	render: function() {
 		return (
-		<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
-		    <path fill="none" d="M0 0h22v22H0z"/>
-		    <path d="M13.293 14.707L10 11.414V6h2v4.586l2.707 2.707-1.414 1.414zM11 2a9 9 0 0 0-9 9h2c0-3.86 3.14-7 7-7s7 3.14 7 7-3.14 7-7 7a6.99 6.99 0 0 1-5.736-3H9v-2H2v7h2v-3.35A8.98 8.98 0 0 0 11 20a9 9 0 0 0 0-18z"/>
-		</svg>
+			<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
+				<path fill="none" d="M0 0h22v22H0z"/>
+				<path d="M13.293 14.707L10 11.414V6h2v4.586l2.707 2.707-1.414 1.414zM11 2a9 9 0 0 0-9 9h2c0-3.86 3.14-7 7-7s7 3.14 7 7-3.14 7-7 7a6.99 6.99 0 0 1-5.736-3H9v-2H2v7h2v-3.35A8.98 8.98 0 0 0 11 20a9 9 0 0 0 0-18z"/>
+			</svg>
 		);
 	}
 } );
