431956fe2641e6f534d789e4f689c811136b3b06
Serge Paquet <spaquet@cortex.bz>
2015-11-23 11:43:00 -0500

    Remove empty icon fill paths

--- lib/icons/arrow-top-right.jsx
+++ lib/icons/arrow-top-right.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function TopRightArrowIcon() {
 	return (
 		<svg className="icon-arrow-top-right" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
-			<path fill="none" d="M0 0h22v22H0z"/>
 			<path d="M6.075 17.364L15 8.464V16h2V5H6v2h7.536L4.65 15.925z"/>
 		</svg>
 	);
--- lib/icons/notes.jsx
+++ lib/icons/notes.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function NotesIcon() {
 	return (
 		<svg className="icon-notes" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
-			<path fill="none" d="M0 0h22v22H0z"/>
 			<path d="M18 4v10H8V4h10m0-2H8a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zM4 18V6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2H4zM16 6h-6v2h6V6zm0 4h-6v2h6v-2z"/>
 		</svg>
 	);
