5170a35c8effa0608ab120e13267945d78e8a762
Serge Paquet <spaquet@cortex.bz>
2015-11-27 15:42:40 -0500

    Add TabbedDialog component

---
+++ lib/tabbed-dialog.jsx
@@ -0,0 +1,52 @@
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+import Dialog from './dialog';
+
+export default React.createClass( {
+
+	propTypes: {
+		tabs: PropTypes.array.isRequired,
+		renderTabName: PropTypes.func.isRequired,
+		renderTabContent: PropTypes.func.isRequired
+	},
+
+	componentWillMount() {
+		this.setState( {
+			currentTab: this.props.tabs[0]
+		} );
+	},
+
+	setCurrentTab( tab ) {
+		this.setState( {
+			currentTab: tab
+		} );
+	},
+
+	render() {
+		var { tabs, renderTabName, renderTabContent, ...dialog } = this.props;
+		var { currentTab } = this.state;
+
+		return (
+			<Dialog className="settings" {...dialog}>
+				<nav className="dialog-tabs">
+					<ul>
+						{tabs.map( tab =>
+							<li key={tab}>
+								<button type="button"
+										className={classNames( 'text-button', { 'dialog-tab-active': tab === currentTab } )}
+										onClick={this.setCurrentTab.bind( this, tab )}>
+									{renderTabName( tab )}
+								</button>
+							</li>
+						)}
+					</ul>
+				</nav>
+
+				<div className="dialog-tab-content">
+					{renderTabContent( this.state.currentTab )}
+				</div>
+			</Dialog>
+		);
+	}
+
+} );
