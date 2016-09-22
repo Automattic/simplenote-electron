e8e8973d9a9f71f71a2bd6cac549c75cd664788c
Dennis Snell <dennis.snell@automattic.com>
2016-09-21 18:12:59 -0700

    Automatically add list bullets on new list items
    Previously, there was a missing behavior in this app that was present in
    the other Simplenote apps: when creating a new line after a line
    starting with a list bullet, the new line should start with the same
    bullet at the same level of indentation.
    
    This PR adds that behavior into the app.

--- lib/note-detail.jsx
+++ lib/note-detail.jsx
@@ -9,7 +9,7 @@ const saveDelay = 2000;
 
 const isValidNote = note => note && note.id;
 const prependTab = l => `\t${ l }`;
-const removeLeadingTab = l => l[0] === '\t' ? l.substring( 1 ) : l;
+const removeLeadingTab = l => l && l.length && l[0] === '\t' ? l.substring( 1 ) : l;
 
 export default class NoteDetail extends Component {
 	static propTypes = {
@@ -54,6 +54,60 @@ export default class NoteDetail extends Component {
 		window.removeEventListener( 'keydown', this.interceptTabPresses );
 	};
 
+	handleKey = event => {
+		if (
+			'Enter' !== event.key ||
+			! this.noteEditor ||
+			this.props.previewingMarkdown ||
+			event.shiftKey ||
+			event.altKey ||
+			event.ctrlKey ||
+			event.metaKey
+		) {
+			return true;
+		}
+
+		const {
+			selectionStart,
+			value,
+		} = this.noteEditor;
+
+		const prevLineStart = value.lastIndexOf( '\n', selectionStart - 1 ) + 1;
+		const prevLine = value.substring( prevLineStart, selectionStart - 1 );
+		const prevIndent = prevLine.match( /^(\s+)([-*])/ );
+
+		// The previous line did not start with
+		// indentation, so continue as normal
+		if ( null === prevIndent ) {
+			return true;
+		}
+
+		const indent = prevIndent[ 1 ];
+		const bullet = prevIndent[ 2 ];
+		const nextIndent = `\n${ indent }${ bullet } `;
+
+		// If going a second time in a row that means we
+		// want to escape the list structure, so unwind
+		// and move back to the beginning of the line
+		if ( prevLine.trim() === nextIndent.trim() ) {
+			this.noteEditor.selectionStart = prevLineStart;
+			this.noteEditor.selectionEnd = Math.min( value.indexOf( '\n', prevLineStart ), value.length );
+			document.execCommand( 'delete', false, null );
+			this.noteEditor.selectionStart = prevLineStart - 1;
+			return true;
+		}
+
+		// Prepend the next list prefix to the line
+		document.execCommand( 'insertText', false, nextIndent );
+		this.noteEditor.selectionStart = selectionStart + nextIndent.length;
+		this.noteEditor.selectionEnd = selectionStart + nextIndent.length;
+
+		event.preventDefault();
+		event.stopPropagation();
+
+		return false;
+	};
+
 	interceptTabPresses = event => {
 		if (
 			'Tab' !== event.code ||
@@ -73,10 +127,31 @@ export default class NoteDetail extends Component {
 		const {
 			selectionStart,
 			selectionEnd,
+			value,
 		} = this.noteEditor;
 
 		// if inserting at a cursor position
 		if ( ! event.shiftKey && selectionStart === selectionEnd ) {
+
+			// handle list indents too
+			const currentLineStart = value.lastIndexOf( '\n', selectionStart - 1 ) + 1;
+			const currentLine = value.substring( currentLineStart, selectionStart );
+			console.log( `currentLine: '${ currentLine }'` );
+
+			// line is a list point
+			if ( /^\s+[-*]\s?$/.test( currentLine ) ) {
+				this.noteEditor.selectionStart = currentLineStart;
+				this.noteEditor.selectionEnd = currentLineStart;
+
+				document.execCommand( 'insertText', false, '\t' );
+
+				this.noteEditor.selectionStart = selectionStart + 1;
+				this.noteEditor.selectionEnd = selectionStart + 1;
+
+				return;
+			}
+
+			// line is not a list point
 			return document.execCommand( 'insertText', false, '\t' );
 		}
 
@@ -117,7 +192,7 @@ export default class NoteDetail extends Component {
 			value,
 		} = this.noteEditor;
 
-		const firstLineStart = value.lastIndexOf( '\n', selectionStart ) + 1;
+		const firstLineStart = value.lastIndexOf( '\n', selectionStart - 1 ) + 1;
 		const lastLineEnd = Math.max( value.indexOf( '\n', selectionEnd ), 0 ) || value.length;
 
 		this.noteEditor.selectionStart = firstLineStart;
@@ -161,6 +236,7 @@ export default class NoteDetail extends Component {
 						className="note-detail-textarea theme-color-bg theme-color-fg"
 						disabled={ !! ( note && note.data.deleted ) }
 						onChange={ this.queueNoteSave }
+						onKeyPress={ this.handleKey }
 						style={ divStyle }
 					/>
 				}
