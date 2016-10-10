1cceb41d21d4faef9be243ac69a024ce23645b60
Nuno Campos <nuno.campos@me.com>
2016-10-10 12:28:44 +0100

    added indent/outdent behaviour to editor

--- lib/note-content-editor.jsx
+++ lib/note-content-editor.jsx
@@ -1,9 +1,31 @@
 import React, { PropTypes } from 'react';
-import { Editor, EditorState, ContentState } from 'draft-js';
+import {
+	Editor, EditorState, ContentState, SelectionState, Modifier
+} from 'draft-js';
 import { invoke, noop } from 'lodash';
 
 function plainTextContent( editorState ) {
 	return editorState.getCurrentContent().getPlainText( '\n' )
+}
+
+const EditorStateModifier = {
+	insertText( editorState, selection, characters ) {
+		return EditorState.push(
+			editorState,
+			Modifier.insertText(
+				editorState.getCurrentContent(), selection, characters
+			),
+			'insert-characters'
+		);
+	},
+
+	removeRange( editorState, selection = editorState.getSelection() ) {
+		return EditorState.push(
+			editorState,
+			Modifier.removeRange( editorState.getCurrentContent(), selection ),
+			'remove-range'
+		);
+	},
 }
 
 export default class NoteContentEditor extends React.Component {
@@ -63,6 +85,78 @@ export default class NoteContentEditor extends React.Component {
 		invoke( this, 'editor.focus' );
 	}
 
+	onTab = ( e ) => {
+		// prevent moving focus to next input
+		e.preventDefault()
+
+		let editorState = this.state.editorState;
+		const selection = editorState.getSelection();
+
+		if ( !selection.isCollapsed() ) {
+			return
+		}
+
+		const content = editorState.getCurrentContent();
+		const selectionStart = selection.getStartOffset();
+		const block = content.getBlockForKey( selection.getFocusKey() );
+		const line = block.getText();
+
+		const atStart = line.trim() === '-' || line.trim() === '*';
+
+		if ( !e.shiftKey ) {
+			// inserting a tab character
+
+			const offset = atStart ? 0 : selectionStart
+
+			// add tab
+			editorState = EditorStateModifier.insertText(
+				editorState,
+				SelectionState.createEmpty( block.getKey() ).merge( {
+					anchorOffset: offset,
+					focusOffset: offset,
+				} ),
+				'\t'
+			);
+
+			// move selection to where it was
+			editorState = EditorState.forceSelection(
+				editorState,
+				SelectionState.createEmpty( block.getKey() ).merge( {
+					anchorOffset: selectionStart + 1, // +1 because 1 char was added
+					focusOffset: selectionStart + 1,
+				} )
+			);
+		} else {
+			// outdenting
+
+			const rangeStart = atStart ? 0 : selectionStart - 1;
+			const rangeEnd = atStart ? 1 : selectionStart;
+			const prevChar = block.getText().slice( rangeStart, rangeEnd );
+
+			if ( prevChar === '\t' ) {
+				// remove tab
+				editorState = EditorStateModifier.removeRange(
+					editorState,
+					SelectionState.createEmpty( block.getKey() ).merge( {
+						anchorOffset: rangeStart,
+						focusOffset: rangeEnd,
+					} )
+				);
+
+				// move selection to where it was
+				editorState = EditorState.forceSelection(
+					editorState,
+					SelectionState.createEmpty( block.getKey() ).merge( {
+						anchorOffset: selectionStart - 1, // -1 because 1 char was removed
+						focusOffset: selectionStart - 1,
+					} )
+				);
+			}
+		}
+
+		this.handleEditorStateChange( editorState );
+	}
+
 	render() {
 		return (
 			<Editor
@@ -71,6 +165,7 @@ export default class NoteContentEditor extends React.Component {
 				stripPastedStyles
 				onChange={this.handleEditorStateChange}
 				editorState={this.state.editorState}
+				onTab={this.onTab}
 			/>
 		);
 	}
