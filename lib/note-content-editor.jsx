6e2fb363fdb552354769d991f62f390f9fef972e
Nuno Campos <nuno.campos@me.com>
2016-10-26 19:22:13 +0100

    added indent/outdent behaviour to editor (#400)
    * added indent/outdent behaviour to editor
    
    * address code style comments
    
    * different organisation
    - got rid of EditorStateModifier
    - created indent/outdentCurrentBlock functions
    - bail out in onTab any modifier key is pressed (other than shift)
    - bail out in handleEditorStateChange if new editorState is referentially equal to the previous one
    
    * support indentation with non-collapsed selections

--- lib/note-content-editor.jsx
+++ lib/note-content-editor.jsx
@@ -1,9 +1,92 @@
 import React, { PropTypes } from 'react';
-import { Editor, EditorState, ContentState } from 'draft-js';
+import {
+	ContentState,
+	Editor,
+	EditorState,
+	Modifier,
+} from 'draft-js';
 import { invoke, noop } from 'lodash';
 
 function plainTextContent( editorState ) {
 	return editorState.getCurrentContent().getPlainText( '\n' )
+}
+
+function indentCurrentBlock( editorState ) {
+	const selection = editorState.getSelection();
+	const selectionStart = selection.getStartOffset();
+
+	const content = editorState.getCurrentContent();
+	const block = content.getBlockForKey( selection.getFocusKey() );
+	const line = block.getText();
+
+	const atStart = line.trim() === '-' || line.trim() === '*';
+	const offset = atStart ? 0 : selectionStart;
+
+	// add tab
+	const afterInsert = EditorState.push(
+		editorState,
+		Modifier.replaceText(
+			content,
+			selection.isCollapsed()
+				? selection.merge( {
+					anchorOffset: offset,
+					focusOffset: offset,
+				} )
+				: selection,
+			'\t'
+		),
+		'insert-characters'
+	);
+
+	// move selection to where it was
+	return EditorState.forceSelection(
+		afterInsert,
+		afterInsert.getSelection().merge( {
+			anchorOffset: selectionStart + 1, // +1 because 1 char was added
+			focusOffset: selectionStart + 1,
+		} )
+	);
+}
+
+function outdentCurrentBlock( editorState ) {
+	const selection = editorState.getSelection();
+	const selectionStart = selection.getStartOffset();
+
+	const content = editorState.getCurrentContent();
+	const block = content.getBlockForKey( selection.getFocusKey() );
+	const line = block.getText();
+
+	const atStart = line.trim() === '-' || line.trim() === '*';
+	const rangeStart = atStart ? 0 : selectionStart - 1;
+	const rangeEnd = atStart ? 1 : selectionStart;
+	const prevChar = block.getText().slice( rangeStart, rangeEnd );
+
+	// there's no indentation to remove
+	if ( prevChar !== '\t' ) {
+		return editorState
+	}
+
+	// remove tab
+	const afterRemove = EditorState.push(
+		editorState,
+		Modifier.removeRange(
+			content,
+			selection.merge( {
+				anchorOffset: rangeStart,
+				focusOffset: rangeEnd,
+			} )
+		),
+		'remove-range'
+	);
+
+	// move selection to where it was
+	return EditorState.forceSelection(
+		afterRemove,
+		selection.merge( {
+			anchorOffset: selectionStart - 1, // -1 because 1 char was removed
+			focusOffset: selectionStart - 1,
+		} )
+	);
 }
 
 export default class NoteContentEditor extends React.Component {
@@ -23,6 +106,10 @@ export default class NoteContentEditor extends React.Component {
 	}
 
 	handleEditorStateChange = ( editorState ) => {
+		if ( editorState === this.state.editorState ) {
+			return
+		}
+
 		const nextContent = plainTextContent( editorState );
 		const prevContent = plainTextContent( this.state.editorState );
 
@@ -63,6 +150,27 @@ export default class NoteContentEditor extends React.Component {
 		invoke( this, 'editor.focus' );
 	}
 
+	onTab = ( e ) => {
+		const { editorState } = this.state;
+
+		// prevent moving focus to next input
+		e.preventDefault()
+
+		if ( ! editorState.getSelection().isCollapsed() && e.shiftKey ) {
+			return
+		}
+
+		if ( e.altKey || e.ctrlKey || e.metaKey ) {
+			return
+		}
+
+		this.handleEditorStateChange(
+			e.shiftKey
+				? outdentCurrentBlock( editorState )
+				: indentCurrentBlock( editorState )
+		)
+	}
+
 	render() {
 		return (
 			<Editor
@@ -71,6 +179,7 @@ export default class NoteContentEditor extends React.Component {
 				stripPastedStyles
 				onChange={this.handleEditorStateChange}
 				editorState={this.state.editorState}
+				onTab={this.onTab}
 			/>
 		);
 	}
