78144c96794563c75d90043a8d0bf411064c7dc5
Dennis Snell <dennis.snell@automattic.com>
2017-01-17 15:56:07 -0700

    Introduce `react-virtualized` to fix performance issues in note list (#476)
    * Introduce `react-virtualized` to fix performance issues in note list
    
    @see https://github.com/bvaughn/react-virtualized
    
    The note list has been a very slow component disproportionately slowing
    down the rest of the app. There were a few reasons for this:
    
     - Every note was being rendered in the note list on every render. This
       meant that each note was processed rendered even if it not actually
       visible to the user.
    
     - Updating the search field quickly sorted the list exacerbating the
       issues with re-rendering all of the notes. Deferring to re-render, on
       the other hand, would obscure the search results.
    
    `react-virtualized` only renders the notes in the note list which would
    be visible and it automatically handles things like overscroll,
    scrolling in general, and deciding how to update the list.
    
    One challenge with Simplenote is that the rows in the note list have
    varying heights depending on the content of the notes themselves. In
    order to make the virtual list work properly, however, we must provide
    the heights of each row.
    
    In this patch we are supplying the height of each row by estimating the
    number of lines of text in the note excerpt. This calculation is
    **extremely fragile** and depends on many other tuned constants such as
    font size, line height, and note list width. That means **this will
    likely be a source of bugs and regressions** moving forward and **we
    need to pay special attention to the row height calculation**.
    
    In several places the behavior of the virtual list is not intuitive and
    therefore there are places in the code which trigger recalculating the
    row heights. This is necessary because the list is a very DOM-stateful
    thing and does not synchronize normally with React as it handles prop
    changes.

--- lib/note-list.jsx
+++ lib/note-list.jsx
@@ -1,17 +1,199 @@
+/**
+ * @module NoteList
+ *
+ * This module includes some ugly code.
+ * The note list display is a significant source of
+ * visual re-render lag for accounts with many notes.
+ * The trade-offs in this file reflect a decision to
+ * optimize heavy inner loops for performance over
+ * code burden.
+ *
+ * Any changes to this code which could affect the
+ * row height calculations should be double-checked
+ * against performance regressions.
+ */
+
 import React, { PropTypes } from 'react';
-import NoteDisplayMixin from './note-display-mixin';
+import { AutoSizer, List } from 'react-virtualized';
 import PublishIcon from './icons/feed';
 import classNames from 'classnames';
-import { get, isEmpty } from 'lodash';
+import { debounce, get, isEmpty } from 'lodash';
 import { connect } from 'react-redux';
 import appState from './flux/app-state';
 import { tracks } from './analytics'
 import filterNotes from './utils/filter-notes';
+import noteTitle from './utils/note-title';
+
+/**
+ * Delay for preventing row height calculation thrashing
+ *
+ * this constant was determined experimentally
+ * and is open to adjustment if it doesn't find
+ * the proper balance between visual updates
+ * and performance impacts recomputing row heights
+ *
+ * @type {Number} minimum number of ms between calls to recomputeRowHeights in virtual/list
+ */
+const TYPING_DEBOUNCE_DELAY = 70;
+
+/** @type {Number} height of title + vertical padding in list rows */
+const ROW_HEIGHT_BASE = 24 + 18;
+
+/** @type {Number} height of one row of preview text in list rows */
+const ROW_HEIGHT_LINE = 21;
+
+/** @type {Object.<String, Number>} maximum number of lines to display in list rows for display mode */
+const maxPreviewLines = {
+	comfy: 1,
+	condensed: 0,
+	expanded: 4,
+};
+
+/**
+ * Uses canvas.measureText to compute and return the width of the given text of given font in pixels.
+ *
+ * @see http://stackoverflow.com/questions/118241/calculate-text-width-with-javascript/21015393#21015393
+ *
+ * @param {String} text The text to be rendered.
+ * @param {String} width width of the containing area in which the text is rendered
+ * @returns {number} width of rendered text in pixels
+ */
+function getTextWidth( text, width ) {
+	const canvas = getTextWidth.canvas || ( getTextWidth.canvas = document.createElement( 'canvas' ) );
+	canvas.width = width;
+	const context = canvas.getContext( '2d' );
+	context.font = '16px arial';
+	return context.measureText( text ).width;
+}
+
+/** @type {Map} stores a cache of computed row heights to prevent re-rendering the canvas calculation */
+const previewCache = new Map();
+
+/** @type {Map} stores a cache of computed note preview excerpts to prevent re-truncating note content */
+const noteCache = new Map();
+
+/**
+ * Caches based on note id and note content
+ *
+ * @param {Function} f sets the value of the cache
+ * @returns {String} note preview excerpt
+ */
+const noteTitleCache = f => note => {
+	const cached = noteCache.get( note.id );
+
+	if ( 'undefined' === typeof cached || note.data.content !== cached[ 0 ] ) {
+		noteCache.set( note.id, [ note.data.content, f( note ) ] );
+	}
+
+	return cached
+		? cached[ 1 ]
+		: noteCache.get( note.id )[ 1 ];
+};
+
+/**
+ * Gets the note preview excerpt
+ *
+ * This is cached by the note id and content
+ *
+ * @function
+ * @param {Object} note note object
+ * @returns {String} note preview excerpt
+ */
+const getNoteTitle = noteTitleCache( note => noteTitle( note ).preview );
+
+/**
+ * Caches based on note id, width, note display format, and note preview excerpt
+ *
+ * @param {Function} f produces the row height
+ * @returns {Number} row height for note in list
+ */
+const rowHeightCache = f => ( notes, { noteDisplay, width } ) => ( { index } ) => {
+	const note = notes[ index ];
+	const preview = getNoteTitle( note );
+
+	const key = notes[ index ].id;
+	const cached = previewCache.get( key );
+
+	if ( 'undefined' !== typeof cached ) {
+		const [ cWidth, cNoteDisplay, cPreview, cHeight ] = cached;
+
+		if ( cWidth === width && cNoteDisplay === noteDisplay && cPreview === preview ) {
+			return cHeight;
+		}
+	}
+
+	const height = f( width, noteDisplay, preview );
+
+	previewCache.set( key, [ width, noteDisplay, preview, height ] );
+
+	return height;
+};
+
+/**
+ * Computes the pixel height of a row for a given preview text in the list
+ *
+ * @param {Number} width how wide the list renders
+ * @param {String} noteDisplay mode of list display: 'comfy', 'condensed', or 'expanded'
+ * @param {String} preview preview snippet from note
+ * @returns {Number} height of the row in the list
+ */
+const computeRowHeight = ( width, noteDisplay, preview ) => {
+	const lines = Math.ceil( getTextWidth( preview, width - 30 ) / ( width - 30 ) );
+	return ROW_HEIGHT_BASE + ROW_HEIGHT_LINE * Math.min( maxPreviewLines[ noteDisplay ], lines );
+};
+
+/**
+ * Estimates the pixel height of a given row in the note list
+ *
+ * This function utilizes a cache to prevent rerendering the text into a canvas
+ * @see rowHeightCache
+ *
+ * @function
+ */
+const getRowHeight = rowHeightCache( computeRowHeight );
+
+/**
+ * Renders an individual row in the note list
+ *
+ * @see react-virtual/list
+ *
+ * @param {Object[]} notes list of filtered notes
+ * @param {String} noteDisplay list view style: comfy, condensed, expanded
+ * @param {Number} selectedNoteId id of currently selected note
+ * @param {Function} onSelectNote used to change the current note selection
+ * @param {Function} onPinNote used to pin a note to the top of the list
+ * @returns {Function} does the actual rendering for the List
+ */
+const renderNote = ( notes, { noteDisplay, selectedNoteId, onSelectNote, onPinNote } ) => ( { index, rowIndex, key, style } ) => {
+	const note = notes[ 'undefined' === typeof index ? rowIndex : index ];
+	const { title, preview } = noteTitle( note );
+	const isPublished = ! isEmpty( note.data.publishURL );
+	const showPublishIcon = isPublished && ( 'condensed' !== noteDisplay );
+
+	const classes = classNames( 'note-list-item', {
+		'note-list-item-selected': selectedNoteId === note.id,
+		'note-list-item-pinned': note.pinned,
+		'published-note': isPublished
+	} );
+
+	return (
+		<div key={key} style={ style } className={classes}>
+			<div className="note-list-item-pinner" tabIndex="0" onClick={onPinNote.bind( null, note )}></div>
+			<div className="note-list-item-text theme-color-border" tabIndex="0" onClick={onSelectNote.bind( null, note.id )}>
+				<div className="note-list-item-title">
+					<span>{ title }</span>
+					{ showPublishIcon &&
+					<div className="note-list-item-published-icon"><PublishIcon /></div> }
+				</div>
+				{ 'condensed' !== noteDisplay && preview.trim() &&
+					<div className="note-list-item-excerpt">{ preview }</div>
+				}
+			</div>
+		</div>
+	);
+};
 
 const NoteList = React.createClass( {
-
-	mixins: [ NoteDisplayMixin ],
-
 	propTypes: {
 		notes: PropTypes.array.isRequired,
 		selectedNoteId: PropTypes.any,
@@ -22,39 +204,70 @@ const NoteList = React.createClass( {
 		showTrash: PropTypes.bool,
 	},
 
+	componentDidMount() {
+		/**
+		 * Prevents rapid changes from incurring major
+		 * performance hits due to row height computation
+		 */
+		this.recomputeHeights = debounce(
+			() => this.list && this.list.recomputeRowHeights(),
+			TYPING_DEBOUNCE_DELAY
+		);
+	},
+
+	componentWillReceiveProps( nextProps ) {
+		if (
+			nextProps.noteDisplay !== this.props.noteDisplay ||
+			nextProps.notes !== this.props.notes
+		) {
+			this.recomputeHeights();
+		}
+	},
+
+	refList( r ) {
+		this.list = r;
+	},
+
 	render() {
-		var { selectedNoteId, onSelectNote, onEmptyTrash, noteDisplay, showTrash } = this.props;
+		const {
+			selectedNoteId,
+			onSelectNote,
+			onEmptyTrash,
+			noteDisplay,
+			showTrash
+		} = this.props;
 
 		const listItemsClasses = classNames( 'note-list-items', noteDisplay );
+
+		const renderNoteRow = renderNote( this.props.notes, {
+			noteDisplay,
+			onSelectNote,
+			onPinNote: this.onPinNote,
+			selectedNoteId,
+		} );
 
 		return (
 			<div className="note-list">
 				<div className={listItemsClasses}>
-					{this.props.notes.map( note => {
-						let text = this.noteTitleAndPreview( note );
-						const isPublished = ! isEmpty( note.data.publishURL );
-						const showPublishIcon = isPublished && ( 'condensed' !== noteDisplay );
-
-						let classes = classNames( 'note-list-item', {
-							'note-list-item-selected': selectedNoteId === note.id,
-							'note-list-item-pinned': note.pinned,
-							'published-note': isPublished
-						} );
-
-						return (
-							<div key={note.id} className={classes}>
-								<div className="note-list-item-pinner" tabIndex="0" onClick={this.onPinNote.bind( this, note )}></div>
-								<div className="note-list-item-text theme-color-border" tabIndex="0" onClick={onSelectNote.bind( null, note.id )}>
-									<div className="note-list-item-title">
-										<span>{text.title}</span>
-										{ showPublishIcon &&
-											<div className="note-list-item-published-icon"><PublishIcon /></div> }
-									</div>
-									<div className="note-list-item-excerpt">{text.preview}</div>
-								</div>
-							</div>
-						);
-					} )}
+					<AutoSizer>
+						{ ( { height, width } ) =>
+							<List
+								ref={ this.refList }
+								estimatedRowSize={ ROW_HEIGHT_BASE + ROW_HEIGHT_LINE * maxPreviewLines[ noteDisplay ] }
+								height={ height }
+								noteDisplay={ noteDisplay }
+								notes={ this.props.notes }
+								rowCount={ this.props.notes.length }
+								rowHeight={ (
+									'condensed' === noteDisplay
+										? ROW_HEIGHT_BASE
+										: getRowHeight( this.props.notes, { noteDisplay, width } )
+								) }
+								rowRenderer={ renderNoteRow }
+								width={ width }
+							/>
+						}
+					</AutoSizer>
 				</div>
 				{!!showTrash &&
 					<div className="note-list-empty-trash theme-color-border">
