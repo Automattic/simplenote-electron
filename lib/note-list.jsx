b3281cf73bc7f15837f627a7ade21842b8054701
Dennis Snell <dennis.snell@automattic.com>
2017-01-19 13:51:38 -0700

    Use CSS to fix bug instead of buggy JS

--- lib/note-list.jsx
+++ lib/note-list.jsx
@@ -311,10 +311,8 @@ const NoteList = React.createClass( {
 		return (
 			<div className="note-list">
 				<div className={listItemsClasses}>
-					<AutoSizer
-						disableWidth
-					>
-						{ ( { height } ) =>
+					<AutoSizer>
+						{ ( { height, width } ) =>
 							<List
 								ref={ this.refList }
 								estimatedRowSize={ ROW_HEIGHT_BASE + ROW_HEIGHT_LINE * maxPreviewLines[ noteDisplay ] }
@@ -325,7 +323,7 @@ const NoteList = React.createClass( {
 								rowHeight={ (
 									'condensed' === noteDisplay
 										? ROW_HEIGHT_BASE
-										: getRowHeight( this.props.notes, { noteDisplay, width: 328 } )
+										: getRowHeight( this.props.notes, { noteDisplay, width } )
 								) }
 								rowRenderer={ renderNoteRow }
 								width={ 328 }
