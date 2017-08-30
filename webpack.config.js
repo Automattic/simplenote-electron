076ac0941714c5006f09071187ef700355381538
Dennis Snell <dennis.snell@automattic.com>
2017-08-31 00:11:06 +0100

    Update build environment versions - webpack1 > webpack3
    This change is mainly about updating `webpack` from the 1.x versions to
    3.x versions. Several secondary changes are the result of that.
    
    The app is now being built with es2015 modules instead of CommonJS
    modules. Theoretically this should shrink the build size and maybe speed
    things up on account of webpack's tree-shaking algorithm and having
    fewer closures in the final code. I haven't tested yet and I don't
    anticipate this being significant as an Electron release.
    
    Several small updates are going on too:
      - The node version has been bumped to 7.9.0 to match that running
        inside of Electron.
      - `package.json` doesn't need `npm bin` to prefix commands
      - I'm trying to let Babel focus on the version of Chrome that Electron
        is running and ignore older browsers.
      - We weren't using `AppCache` and didn't need it. It's a deprecated
        API anyway.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const autoprefixer = require( 'autoprefixer' );
 const webpack = require( 'webpack' );
-const AppCachePlugin = require( 'appcache-webpack-plugin' );
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 const config = require( './get-config' );
 const spawnSync = require( 'child_process' ).spawnSync;
@@ -16,25 +15,48 @@ module.exports = {
 		filename: 'app.js'
 	},
 	module: {
-		preLoaders: [
-			{ test: /\.jsx?$/, exclude: /node_modules|lib\/simperium/, loaders: [ 'eslint-loader' ] }
-		],
-		loaders: [
-			{ test: /\.jsx?$/, exclude: /node_modules/, loaders: [ 'babel' ] },
-			{ test: /\.json$/, loader: 'json-loader'},
-			{ test: /\.scss$/, loader: 'style-loader!css-loader!postcss-loader!sass-loader'}
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules|lib\/simperium/,
+				enforce: 'pre',
+				use: [ {
+					loader: 'eslint-loader',
+					options: {
+						cache: true
+					}
+				} ]
+			},
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: [ {
+					loader: 'babel-loader',
+					options: {
+						cacheDirectory: true
+					}
+				} ]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					{ loader: 'postcss-loader', options: { plugins: [ autoprefixer() ] } },
+					'sass-loader'
+				]
+			}
 		]
 	},
 	resolve: {
-		extensions: ['', '.js', '.jsx', '.json', '.scss', '.css' ],
-		moduleDirectories: [ 'lib', 'node_modules' ]
+		extensions: [ '.js', '.jsx', '.json', '.scss', '.css' ],
+		modules: [ 'node_modules' ]
 	},
 	plugins: [
 		new webpack.DllReferencePlugin( {
 			context: process.cwd(),
 			manifest: require( process.cwd() + '/dist/vendor.json' )
 		} ),
-		new AppCachePlugin(),
 		new HtmlWebpackPlugin( {
 			'build-platform': process.platform,
 			'build-reference': spawnSync( 'git', [ 'describe', '--always', '--dirty' ] ).stdout.toString( 'utf8' ).replace( '\n', '' ),
@@ -44,11 +66,8 @@ module.exports = {
 			title: 'Simplenote',
 		} ),
 		new webpack.DefinePlugin( {
-			'process.env.NODE_ENV': JSON.stringify(
-				process.env.NODE_ENV || 'development'
-			),
+			'process.env.NODE_ENV': JSON.stringify( process.env.NODE_ENV || 'development' ),
 			config: JSON.stringify( config() )
 		} )
-	],
-	postcss: [ autoprefixer() ]
+	]
 };
