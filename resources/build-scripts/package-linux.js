aa8d8a81861de9a442c1d352d4c6dcf7a093a115
Maciej Czekaj <natanielcz@gmail.com>
2017-10-27 21:28:58 +0200

    #422 Added support for building rpm installer

--- resources/build-scripts/package-linux.js
+++ resources/build-scripts/package-linux.js
@@ -1,55 +1,58 @@
+/* eslint-disable no-console */
 'use strict';
+
+var onErrorBail = function(error) {
+  if (error) {
+    console.log('Error: ' + error);
+    process.exit(1);
+  }
+};
 
 /**
  * External Dependencies
  */
-var path = require( 'path' );
-var fs = require( 'fs' );
+const path = require('path');
+const fs = require('fs');
 
 /**
  * Internal dependencies
  */
-var config = require( '../lib/config' );
-var cp = require('child_process');
+const config = require('../lib/config');
+const cp = require('child_process');
 
 /**
  * Module variables
  */
 console.log('Building Linux package...');
 
-var onErrorBail = function( error ) {
-	if (error) {
-		console.log("Error: " + error);
-		process.exit(1);
-	}
-};
-
 // copy build into place for packaging
-cp.execSync( "rm -rf release/tmp", onErrorBail ); // clean start
-cp.execSync( "mkdir -p release/tmp/usr/share/applications", onErrorBail );
-cp.execSync( "mkdir -p release/tmp/usr/share/pixmaps", onErrorBail );
-cp.execSync( "cp -r release/Simplenote-linux-x64 release/tmp/usr/share/simplenote", onErrorBail );
-cp.execSync( "mv release/tmp/usr/share/simplenote/Simplenote release/tmp/usr/share/simplenote/simplenote", onErrorBail );	// rename binary to wpcom
-cp.execSync( "cp resources/linux/simplenote.desktop release/tmp/usr/share/applications/", onErrorBail );
-cp.execSync( "cp resources/images/icon_256x256.png release/tmp/usr/share/pixmaps/simplenote.png", onErrorBail );
+cp.execSync('rm -rf release/tmp', onErrorBail); // clean start
+cp.execSync('mkdir -p release/tmp/usr/share/applications', onErrorBail);
+cp.execSync('mkdir -p release/tmp/usr/share/pixmaps', onErrorBail);
+cp.execSync(
+  'cp -r release/Simplenote-linux-x64 release/tmp/usr/share/simplenote',
+  onErrorBail
+);
+cp.execSync(
+  'mv release/tmp/usr/share/simplenote/Simplenote release/tmp/usr/share/simplenote/simplenote',
+  onErrorBail
+); // rename binary to wpcom
+cp.execSync(
+  'cp resources/linux/simplenote.desktop release/tmp/usr/share/applications/',
+  onErrorBail
+);
+cp.execSync(
+  'cp resources/images/icon_256x256.png release/tmp/usr/share/pixmaps/simplenote.png',
+  onErrorBail
+);
 
-var cmd = [
-	'fpm',
-	'--version '  + config.version,
-	'--license "GPLv2"',
-	'--name "simplenote"',
-	'--description "Simplenote for Linux"',
-	'--vendor "Automattic, Inc."',
-	'--maintainer "Simplenote Support <support@simplenote.com>"',
-	'--url "https://simplenote.com/"',
-	'-s dir',
-	'-t deb',
-	'--force', 			// forces overwrite of existing package
-	'--package ./release/simplenote-' + config.version + '.deb',
-	'-C release/tmp',	// starts file search here
-	'./'
-];
+for (let type of ['deb', 'rpm']) {
+  const cmd =
+    `fpm --version ${config.version} --license "${config.license}" --name "${config.name.toLowerCase()}" ` +
+    `--description "${config.description}" --vendor "${config.vendor}" --maintainer "${config.maintainer}" --url "${config.website}" ` +
+    `-s dir -t ${type} --force --package ./release/simplenote-${config.version}.${type} -C release/tmp ./`;
 
-cp.execSync( cmd.join(' '), onErrorBail );
+  cp.execSync(cmd, onErrorBail);
+}
 
-cp.execSync( "rm -rf release/tmp", onErrorBail );
+cp.execSync('rm -rf release/tmp', onErrorBail);
