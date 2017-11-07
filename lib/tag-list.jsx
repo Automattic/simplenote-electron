7c4297b7782cc945d5777c9020bfb0754b0354c0
Dan Roundhill <dan@automattic.com>
2017-11-06 16:09:57 -0800

    Fixes '0' from displaying near the tags edit button (#654)
    * Fixed a `0` character from displaying near the tags list as notes/tags are indexed.
    
    * Check for `> 0` instead.

--- lib/tag-list.jsx
+++ lib/tag-list.jsx
@@ -64,7 +64,7 @@ export class TagList extends Component {
       <div className={classes}>
         <div className="tag-list-title">
           <h2 className="panel-title theme-color-fg-dim">Tags</h2>
-          {tags.length && (
+          {tags.length > 0 && (
             <button
               className="tag-list-edit-toggle button button-borderless"
               tabIndex="0"
