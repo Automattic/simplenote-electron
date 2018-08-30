62bb22607d12c98de0667b6d4204be509d841c82
Lena Morita <lena@jaguchi.com>
2018-08-30 21:24:31 +0900

    Split out menu utils and add tests

--- desktop/menus/view-menu.js
+++ desktop/menus/view-menu.js
@@ -1,26 +1,4 @@
-const buildRadioGroup = ({ action, propName, settings }) => {
-  return item => {
-    const { id, ...props } = item;
-
-    return {
-      type: 'radio',
-      checked: id === settings[propName],
-      click: appCommandSender({
-        action,
-        [propName]: id,
-      }),
-      ...props,
-    };
-  };
-};
-
-const appCommandSender = arg => {
-  return (item, focusedWindow) => {
-    if (focusedWindow) {
-      focusedWindow.webContents.send('appCommand', arg);
-    }
-  };
-};
+const { buildRadioGroup, appCommandSender } = require('./utils');
 
 const buildViewMenu = settings => {
   settings = settings || {};
