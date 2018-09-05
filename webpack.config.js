cc1f885915dcc601542ab59245043b0f70e57c81
Lena Morita <lena@jaguchi.com>
2018-09-05 21:48:06 +0900

    Prepare consolidation of component styles

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,7 @@ module.exports = {
           {
             loader: 'sass-loader',
             options: {
+              includePaths: [__dirname + '/lib'],
               sourceMap: true,
             },
           },
