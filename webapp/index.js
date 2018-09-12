bef705bd806b96ef07e593f7e5ee97ad9f44a0ee
Lena Morita <lena@jaguchi.com>
2018-09-12 18:58:09 +0900

    Set up vip-go wrapper

--- webapp/index.js
+++ webapp/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const { server } = require('@automattic/vip-go');
+
+const port = process.env.PORT || 4000;
 
 app.use(express.static('dist'));
-app.listen(3000);
+
+server(app, { PORT: port }).listen();
