31670f644d3edcd8aa6a92a3d4f9e8df3c9907cd
Stefan Malzner <stefan@adlk.io>
2018-09-17 15:05:51 +0200

    Auto format file

--- desktop/updater/auto-updater/index.js
+++ desktop/updater/auto-updater/index.js
@@ -5,58 +5,65 @@
  */
 const EventEmitter = require('events').EventEmitter;
 const util = require('util');
-const electron = require( 'electron' );
+const electron = require('electron');
 const autoUpdater = electron.autoUpdater;
 const dialog = electron.dialog;
 
 /**
  * Internal dependencies
  */
-const AppQuit = require( '../..//app-quit' );
+const AppQuit = require('../../app-quit');
 
-function AutoUpdater( url ) {
-	this.hasPrompted = false;
-	this.url = url;
+function AutoUpdater(url) {
+  this.hasPrompted = false;
+  this.url = url;
 
-	autoUpdater.on( 'error', () => this.emit( 'end' ) );
-	autoUpdater.on( 'update-not-available', () => this.emit( 'end' ) );
-	autoUpdater.on( 'update-downloaded', this.onDownloaded.bind( this ) );
+  autoUpdater.on('error', () => this.emit('end'));
+  autoUpdater.on('update-not-available', () => this.emit('end'));
+  autoUpdater.on('update-downloaded', this.onDownloaded.bind(this));
 
-	try {
-		autoUpdater.setFeedURL( url );
-	} catch (e) {
-		console.log( e.message );
-	}
+  try {
+    autoUpdater.setFeedURL(url);
+  } catch (e) {
+    console.log(e.message);
+  }
 }
 
 util.inherits(AutoUpdater, EventEmitter);
 
 AutoUpdater.prototype.ping = function() {
-	autoUpdater.checkForUpdates();
+  autoUpdater.checkForUpdates();
 };
 
-AutoUpdater.prototype.onDownloaded = function( event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate ) {
-	const updateDialogOptions = {
-		buttons: [ 'Update & Restart', 'Cancel' ],
-		title: 'Update Available',
-		message: releaseName,
-		detail: releaseNotes
-	};
+AutoUpdater.prototype.onDownloaded = function(
+  event,
+  releaseNotes,
+  releaseName,
+  releaseDate,
+  updateUrl,
+  quitAndUpdate
+) {
+  const updateDialogOptions = {
+    buttons: ['Update & Restart', 'Cancel'],
+    title: 'Update Available',
+    message: releaseName,
+    detail: releaseNotes,
+  };
 
-	if ( this.hasPrompted === false ) {
-		this.hasPrompted = true;
+  if (this.hasPrompted === false) {
+    this.hasPrompted = true;
 
-		dialog.showMessageBox( updateDialogOptions, button => {
-			this.hasPrompted = false;
+    dialog.showMessageBox(updateDialogOptions, button => {
+      this.hasPrompted = false;
 
-			if ( button === 0 ) {
-				AppQuit.allowQuit();
-				quitAndUpdate();
-			} else {
-				this.emit( 'end' );
-			}
-		} );
-	}
+      if (button === 0) {
+        AppQuit.allowQuit();
+        quitAndUpdate();
+      } else {
+        this.emit('end');
+      }
+    });
+  }
 };
 
 module.exports = AutoUpdater;
