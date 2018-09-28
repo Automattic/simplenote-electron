a70ddc1e548ae15e0627d91aa0e697504b759964
Lena Morita <lena@jaguchi.com>
2018-09-28 19:32:38 +0900

    Add Sidebar toggle button (#881)
    * Refactor icon-button in note-toolbar
    
    * Tweak icon-button disabled state
    
    * Refactor icon-button in search-bar
    
    * Refactor icon buttons in SearchBar and NoteToolbar (#880)
    
    * Add Sidebar toggle button (#881)
    
    * Add Sidebar toggle button
    
    * Remove unnecessary min-width
    
    * Animate NoteList (slide out, fade in)
    
    * Animate NoteList variation (slide out, slide in)
    
    * Don't persist Focus Mode setting
    
    * Revert "Animate NoteList variation (slide out, slide in)"
    
    This reverts commit 4dbda20f6870ee48115e6b51c41206b6976bc5bb.

---
+++ lib/icons/sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function SidebarIcon() {
+  return (
+    <svg
+      className="icon-sidebar"
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+    >
+      <path d="M19,3H3C1.895,3,1,3.895,1,5v12c0,1.105,0.895,2,2,2h16c1.105,0,2-0.895,2-2V5C21,3.895,20.105,3,19,3z M3,5h3v12H3V5z
+        M19,17H8V5h11V17z" />
+    </svg>
+  );
+}
