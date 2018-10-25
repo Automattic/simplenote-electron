66f6b5dcbaa5dd357a5e0eacb6e4971b2cc72bcd
Lena Morita <lena@jaguchi.com>
2018-10-26 07:22:39 +0900

    Add ImportExecutor

---
+++ lib/dialogs/import/source-importer/executor.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import PanelTitle from '../../../components/panel-title';
+
+class ImportExecutor extends React.Component {
+  static propTypes = {
+    hint: PropTypes.string,
+    startImport: PropTypes.func.isRequired,
+  };
+
+  state = {
+    setMarkdown: false,
+  };
+
+  render() {
+    const { hint, startImport } = this.props;
+    const { setMarkdown } = this.state;
+
+    return (
+      <div className="source-importer-executor">
+        <section className="source-importer-executor__options">
+          <PanelTitle headingLevel="3">Options</PanelTitle>
+          <label>
+            <input
+              type="checkbox"
+              checked={setMarkdown}
+              className="source-importer-executor__checkbox"
+              onChange={() => this.setState({ setMarkdown: !setMarkdown })}
+            />
+            Enable Markdown on all notes
+          </label>
+          {hint && <p className="theme-color-fg-dim">{hint}</p>}
+        </section>
+        <div className="source-importer-executor__button">
+          <button
+            className="button button-primary"
+            type="button"
+            onClick={() => startImport({ markdown: setMarkdown })}
+          >
+            Import
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ImportExecutor;
