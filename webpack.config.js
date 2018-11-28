e1e964a265875c15b0215477915e7046c3e82c87
Lena Morita <lena@jaguchi.com>
2018-11-29 04:20:56 +0900

    Polyfill app and node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -40,13 +40,9 @@ module.exports = () => {
         },
         {
           test: /\.jsx?$/,
-          exclude: /node_modules/,
           use: [
             {
               loader: 'babel-loader',
-              options: {
-                cacheDirectory: true,
-              },
             },
           ],
         },
