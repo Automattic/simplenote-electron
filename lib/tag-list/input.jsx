59e9c14c774260f5f083c26dd1cc73f5b8bb8ab0
Lena Morita <lena@jaguchi.com>
2018-12-27 01:06:48 +0900

    Make TagListInput a controlled component
    Fixes UI lag when renaming tags (#1118)

---
+++ lib/tag-list/input.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+export class TagListInput extends Component {
+  static propTypes = {
+    editable: PropTypes.bool.isRequired,
+    isSelected: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired,
+    onDone: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { value: props.value };
+  }
+
+  render() {
+    const { editable, isSelected, onClick, onDone } = this.props;
+    const { value } = this.state;
+    const classes = classNames('tag-list-input', 'theme-color-fg', {
+      active: isSelected,
+    });
+
+    return (
+      <input
+        className={classes}
+        readOnly={!editable}
+        onClick={onClick}
+        value={value}
+        onChange={e => this.setState({ value: e.target.value })}
+        onBlur={onDone}
+        spellCheck={false}
+      />
+    );
+  }
+}
+
+export default TagListInput;
