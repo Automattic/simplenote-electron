d8c3c359abc6ff51aac1907250ee81bf31832db7
belcherj <belcherj@gmail.com>
2019-06-07 09:59:50 -0400

    Lint fixes from prettier update

--- desktop/env.js
+++ desktop/env.js
@@ -1,3 +1,6 @@
 module.exports = {
-  isDev: process.env.NODE_ENV === 'development' || process.defaultApp || /node_modules[\\/]electron[\\/]/.test(process.execPath)
-}
+  isDev:
+    process.env.NODE_ENV === 'development' ||
+    process.defaultApp ||
+    /node_modules[\\/]electron[\\/]/.test(process.execPath),
+};
