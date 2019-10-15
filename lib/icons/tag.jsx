e7f26956b9e7805002885281512330fe1a03c556
Kat Hagan <kat@codebykat.com>
2019-10-15 14:14:08 -0700

    added single tag icon

--- lib/icons/tag.jsx
+++ lib/icons/tag.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-// TODO we need a single tag icon
 export default function TagIcon() {
   return (
     <svg
       className="icon-tag"
       xmlns="http://www.w3.org/2000/svg"
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
     >
-      <path d="M20 5v5.172l-7 7L7.828 12l7-7H20m2-2h-8l-9 9 8 8 9-9V3zM3.763 13.237L2.525 12l1.237-1.237L11.525 3H9l-9 9 8 8 1.263-1.263-5.5-5.5zM17.5 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+      <rect x="0" fill="none" width="24" height="24" />
+      <path d="M18.29,5.71v5.17l-7,7L6.12,12.71l7-7h5.17m2-2h-8L4,12a1,1,0,0,0,0,1.41L10.59,20A1,1,0,0,0,12,20l8.29-8.29v-8Zm-4.5,5.5a1,1,0,1,0-1-1A1,1,0,0,0,15.79,9.21Z" />
     </svg>
   );
 }
