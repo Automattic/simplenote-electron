f703f1b1c70f7e7a9225866c6cc333bbc91108d0
Dennis Snell <dennis.snell@automattic.com>
2019-11-14 08:04:16 -0600

    Crash on missing platform support (#1713)
    The Simplenote app, for better or worse, currently depends on a few
    storage technologies that may be disabled through browser settings,
    extensions, and manual configuration.
    
    When these dependencies are missing we cannot function normally and
    right now the app crashes or breaks in misleading ways.
    
    In this patch we perform an up-front check when the app boots to verify
    that we have some basic dependencies met. This is not an exhaustive
    validation but it should catch the most common cases and give customers
    an idea of why things may be broken.

--- lib/components/boot-warning/index.jsx
+++ lib/components/boot-warning/index.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import './style';
 
-const BootWarning = () => {
-  return (
-    <h3 className="boot-warning__message">
-      Simplenote cannot be opened simultaneously in more than one tab or window
-      per browser.
-    </h3>
-  );
-};
+const BootWarning = ({ children }) => (
+  <h3 className="boot-warning__message">{children}</h3>
+);
 
 export default BootWarning;
