2a131c5b1f199631ab7938d9d04997a05272ab6f
Dennis Snell <dennis.snell@automattic.com>
2020-01-07 11:09:25 -0700

    Add missing decorators in note list previews (#1814)
    When showing each item in the note list we run the content decorators on them
    in order to show checkboxes instead of the markdown syntax for a checkbox and
    we also highlight the matching text from the search box.
    
    Previously we have only been showing the first match for each decorator because
    of a couple small bugs:
     - the task prefix matcher wasn't using the `m` multi-line flag in its RegExp
       pattern
     - the search filter text was only replacing the first occurrance, see JS
       "replaceAll()" proposal
    
    In this patch we're rewritten `decorateWith` to automatically create a global
    RegExp pattern for the search text and the task prefix RegExp pattern has been
    corrected.
    
    After this patch you can now see all of the decorated items in the note list;
    for example, if you had two checklist items in a note then you can now see
    multiple checkboxes in the note list whereas before you'd only see the first
    one and then the second one would appear as ` - [ ]` or `- [x]`

--- lib/note-list/decorators.tsx
+++ lib/note-list/decorators.tsx
@@ -1,37 +1,41 @@
 import React from 'react';
+import { escapeRegExp } from 'lodash';
 import replaceToArray from 'string-replace-to-array';
 
 import Checkbox from '../components/checkbox';
+import { withoutTags } from '../utils/filter-notes';
 import { taskPrefixRegex } from '../note-detail/toggle-task/constants';
 
-export const decorateWith = (decorators, text) => {
-  let output = text;
-  let i = 0;
+export const decorateWith = (decorators, text) =>
+  decorators
+    .reduce((output, { filter, replacer }) => {
+      const searchText = 'string' === typeof filter && withoutTags(filter);
+      const pattern =
+        searchText && searchText.length > 0
+          ? new RegExp(escapeRegExp(searchText), 'g')
+          : filter;
 
-  decorators.forEach(({ filter, replacer }) => {
-    output = replaceToArray(output, filter, replacer(i++));
-  });
-
-  return output;
-};
+      return replaceToArray(output, pattern, replacer);
+    }, text)
+    .map((chunk, key) =>
+      chunk && 'string' !== typeof chunk
+        ? React.cloneElement(chunk, { key })
+        : chunk
+    );
 
 export const checkboxDecorator = {
   filter: taskPrefixRegex,
-  replacer: key => match => {
+  replacer: match => {
     const isChecked = /x/i.test(match);
-    return <Checkbox checked={isChecked} key={key} />;
+    return <Checkbox checked={isChecked} />;
   },
 };
 
 export const makeFilterDecorator = filter => ({
   filter,
-  replacer: key => match => {
+  replacer: match => {
     if (match.length) {
-      return (
-        <span className="search-match" key={key}>
-          {match}
-        </span>
-      );
+      return <span className="search-match">{match}</span>;
     }
   },
 });
