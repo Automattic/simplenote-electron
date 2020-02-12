0c8b34908efe65a43abde4f9593b450051781022
Jonathan (JB) Belcher <belcherj@gmail.com>
2020-02-12 12:56:04 -0500

    Add return type

--- lib/components/sync-status/get-note-titles.ts
+++ lib/components/sync-status/get-note-titles.ts
@@ -3,8 +3,8 @@ import noteTitleAndPreview from '../../utils/note-utils';
 import * as T from '../../types';
 
 type NoteTitle = {
-  id: String;
-  title: String;
+  id: T.EntityId;
+  title: string;
 };
 
 const getNoteTitles = (
@@ -12,7 +12,7 @@ const getNoteTitles = (
   notes: T.NoteEntity[] = [],
   limit: number = Infinity
 ) => {
-  const matchedNotes = ids.reduce((acc: NoteTitle[], id) => {
+  const matchedNotes = ids.reduce((acc: NoteTitle[], id): NoteTitle[] => {
     const note = notes.find(thisNote => thisNote.id === id);
     return note
       ? [...acc, { id, title: noteTitleAndPreview(note).title }]
