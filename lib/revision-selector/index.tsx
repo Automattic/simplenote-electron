8e4633ff1ef867806e528f5ab20834121c34a680
Jonathan Belcher <belcherj@gmail.com>
2020-02-20 09:21:03 -0500

    Fix revisions loading on revisions panel toggle (#1915)
    * Fix revisions loading on revisions panel toggle

--- lib/revision-selector/index.tsx
+++ lib/revision-selector/index.tsx
@@ -175,7 +175,6 @@ export class RevisionSelector extends Component<Props, ComponentState> {
 }
 
 const mapStateToProps: S.MapState<StateProps> = ({
-  appState: state,
   ui: { note, showRevisions },
 }) => ({
   isViewingRevisions: showRevisions,
