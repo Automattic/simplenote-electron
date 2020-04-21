59d4df86dea1bf1d1e6a03b9f4fa61a3a940732a
Jonathan Belcher <belcherj@gmail.com>
2020-04-21 00:14:58 -0400

    Types: TagChip

--- lib/components/tag-chip/index.tsx
+++ lib/components/tag-chip/index.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import { noop } from 'lodash';
 
-export const TagChip = ({ onSelect = noop, selected, tag: tagName }) => (
+type OwnProps = {
+  onSelect: () => {};
+  selected: boolean;
+  tag: string;
+};
+
+const TagChip: FunctionComponent<OwnProps> = ({ onSelect, selected, tag }) => (
   <div
     className={classNames('tag-chip', { selected })}
-    data-tag-name={tagName}
+    data-tag-name={tag}
     onClick={onSelect}
   >
-    {tagName}
+    {tag}
   </div>
 );
-
-TagChip.displayName = 'TagChip';
-
-TagChip.propTypes = {
-  onSelect: PropTypes.func,
-  selected: PropTypes.bool,
-  tag: PropTypes.string.isRequired,
-};
 
 export default TagChip;
