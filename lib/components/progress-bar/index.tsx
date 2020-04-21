c4bc134017e7407d8272c6b30f3bff934c568cbf
Jonathan Belcher <belcherj@gmail.com>
2020-04-20 22:55:48 -0400

    Code review update

--- lib/components/progress-bar/index.tsx
+++ lib/components/progress-bar/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { LinearProgress, LinearProgressProps } from '@material-ui/core';
+import { LinearProgress } from '@material-ui/core';
 
-const ProgressBar: React.FunctionComponent<LinearProgressProps> = (props) => {
+const ProgressBar: typeof LinearProgress = (props) => {
   return (
     <LinearProgress
       classes={{
