9c54b6c4ce4b910a1f2caa715bd9edfdde65755f
Jonathan (JB) Belcher <belcherj@gmail.com>
2020-04-27 10:50:59 -0400

    Update type

--- lib/components/tag-chip/index.tsx
+++ lib/components/tag-chip/index.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 import classNames from 'classnames';
 
 type OwnProps = {
-  onSelect: () => {};
+  onSelect: () => any;
   selected: boolean;
   tag: string;
 };
