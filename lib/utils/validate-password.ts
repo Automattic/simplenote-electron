21814546b92b439051aec742f7a24711c203f7ad
Jonathan (JB) Belcher <belcherj@gmail.com>
2020-05-14 10:26:48 -0400

    Add Validate Password util

---
+++ lib/utils/validate-password.ts
@@ -0,0 +1,10 @@
+export const validatePassword = function (password: string, email: string) {
+  // does not contain username (i.e. email address)
+  if (email !== '' && password.includes(email)) {
+    return false;
+  }
+  // minimum of 8 characters, max of 64
+  // allow symbols ~!@#$%^&*_-+=`|(){}[]:;"',.?/
+  const re = /^[a-zA-Z0-9~ !@#$%^&*_\-+=`|()[\]:;"',.?\/]{8,64}$/;
+  return re.test(password);
+};
