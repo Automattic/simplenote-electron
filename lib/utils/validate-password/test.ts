b516061ce74cb6b1546bb4940efd3cc9979bb2e5
Kat Hagan <kat.hagan@automattic.com>
2020-05-21 12:17:08 -0700

    Update: Passwords can contain but not equal email, remove max character limit (#2112)
    * policy updates: cannot EQUAL email, over 64 characters okay
    
    * update password error

--- lib/utils/validate-password/test.ts
+++ lib/utils/validate-password/test.ts
@@ -1,26 +1,21 @@
 import { validatePassword } from './';
 
-describe('validatePaswword', () => {
+describe('validatePassword', () => {
   it('should return false for password that is too short', () => {
     expect(validatePassword('foo', 'bar@bang.com')).toBeFalsy();
   });
 
-  it('should return false for password that contains email that would otherwise be valid', () => {
-    expect(
-      validatePassword(
-        'bar@bang.com*xz8-X6uskm8ZjhLXvTTw2m8YxgvgujuWpEqUqqdT!H4',
-        'bar@bang.com'
-      )
-    ).toBeFalsy();
+  it('should return false for password that equals email that would otherwise be valid', () => {
+    expect(validatePassword('bar@bang.com', 'bar@bang.com')).toBeFalsy();
   });
 
-  it('should return false for password that is more than 64 characters', () => {
+  it('should return true for password that is more than 64 characters', () => {
     expect(
       validatePassword(
         'y93HywVqX6sXbsyZiAAZ*xz8-X6uskm8ZjhLXvTTw2m8YxgvgujuWpEqUqqdT!H41111',
         'bar@bang.com'
       )
-    ).toBeFalsy();
+    ).toBeTruthy();
   });
 
   it('should return true for password that is numbers only', () => {
