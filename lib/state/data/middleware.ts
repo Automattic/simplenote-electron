fbe12f2c7e7a80b2a55341f75213a3dbe988b4a9
Dennis Snell <dennis.snell@automattic.com>
2020-05-27 14:09:52 -0700

    add test/prefill

--- lib/state/data/middleware.ts
+++ lib/state/data/middleware.ts
@@ -38,4 +38,26 @@ export const middleware: S.Middleware = (store) => (
   }
 };
 
+if ('development' === process.env.NODE_ENV) {
+  window.prefill = () =>
+    Promise.all([
+      import('../../utils/import/simplenote'),
+      import('../../utils/import'),
+      import('./test_account.json'),
+    ]).then(([Simplenote, CoreImporter, data]) => {
+      const notes = {
+        activeNotes: Simplenote.convertModificationDates(data.activeNotes),
+        trashedNotes: Simplenote.convertModificationDates(data.trashedNotes),
+      };
+
+      new CoreImporter.default((note) =>
+        window.dispatch({
+          type: 'IMPORT_NOTE_WITH_ID',
+          noteId: uuid(),
+          note,
+        })
+      ).importNotes(notes);
+    });
+}
+
 export default middleware;
