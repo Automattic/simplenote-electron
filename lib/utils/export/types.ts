8a868870bdce465f980347d0717e702719b05e07
Jonathan Belcher <belcherj@gmail.com>
2020-06-30 21:13:23 -0400

    Remove uneeded type

--- lib/utils/export/types.ts
+++ lib/utils/export/types.ts
@@ -13,6 +13,6 @@ export type ExportNote = {
 };
 
 export type GroupedExportNotes = {
-  activeNotes: ExportNote[] | [];
-  trashedNotes: ExportNote[] | [];
+  activeNotes: ExportNote[];
+  trashedNotes: ExportNote[];
 };
