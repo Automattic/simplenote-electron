26f250ab5c5314fabb2d86f3008576557f4dd90c
Kat Hagan <kat@codebykat.com>
2020-07-28 17:18:22 -0700

    don't display bar on Electron

--- lib/components/beta-bar/index.tsx
+++ lib/components/beta-bar/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import SmallCrossIcon from '../../icons/cross-small';
+import { isElectron } from '../../utils/platform';
 
 const BetaBar = () => {
   const [isVisible, setIsVisible] = useState(true);
-  return isVisible ? (
+
+  return !isElectron && isVisible ? (
     <div className="beta-bar">
       You&lsquo;re invited to try Simplenote Beta.
       <a href="https://staging.simplenote.com">Try it now.</a>
