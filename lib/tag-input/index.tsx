87ab0f59ba9b4ff14a046657ff6aca49351a225b
Sandy McFadden <sandy.mcfadden@automattic.com>
2021-01-28 09:59:50 -0400

    Undoing last commit. Going to address in a separate PR instead

--- lib/tag-input/index.tsx
+++ lib/tag-input/index.tsx
@@ -13,7 +13,6 @@ import type * as T from '../types';
 
 const KEY_TAB = 9;
 const KEY_ENTER = 13;
-const KEY_SPACE = 32;
 const KEY_RIGHT = 39;
 const KEY_COMMA = 188;
 
@@ -124,7 +123,6 @@ export class TagInput extends Component<Props> {
       {
         [KEY_ENTER]: this.submitTag,
         [KEY_COMMA]: this.submitTag,
-        [KEY_SPACE]: this.submitTag,
         [KEY_TAB]: this.interceptTabPress,
         [KEY_RIGHT]: this.interceptRightArrow,
       },
