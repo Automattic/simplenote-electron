c324e9fc2874863db622a03e33d7dbf2f000540f
David Calhoun <dpcalhoun@gmail.com>
2021-03-09 08:24:06 -0600

    Refactor for code style preference

--- lib/components/dev-badge/index.tsx
+++ lib/components/dev-badge/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const DevBadge = () => {
-  return (
-    <div aria-hidden="true" className="dev-badge">
-      DEV
-    </div>
-  );
-};
+const DevBadge = () => (
+  <div aria-hidden="true" className="dev-badge">
+    DEV
+  </div>
+);
 
 export default DevBadge;
