a682de65213b38de0ea8796d688934d1d29bc60e
Klauswk <klauswk1@hotmail.com>
2021-03-23 09:52:50 -0300

    Fix unnecessary separators in login (#2724)
    Fix unnecessary separators in the Electron builds File and Edit menus when not yet logged in.

--- desktop/menus/edit-menu.js
+++ desktop/menus/edit-menu.js
@@ -23,59 +23,76 @@ const buildEditMenu = (settings, isAuthenticated, editMode) => {
     accelerator: 'CommandOrControl+A',
   };
 
+  const editModeMenuOptions = editMode
+    ? [
+        undo,
+        redo,
+        {
+          type: 'separator',
+        },
+      ]
+    : [];
+
   // menu items with roles don't respect visibility, so we have to do this the hard way
   if (!editMode) {
     selectAll['role'] = 'selectAll';
   }
 
-  return {
-    label: '&Edit',
-    submenu: [
-      undo,
-      redo,
-      {
-        type: 'separator',
-      },
-      {
-        label: '&Cut',
-        role: 'cut',
-      },
-      {
-        label: 'C&opy',
-        role: 'copy',
-      },
-      {
-        label: '&Paste',
-        role: 'paste',
-      },
-      selectAll,
+  let authenticatedMenuOptions = [];
+
+  if (isAuthenticated) {
+    authenticatedMenuOptions = [
       { type: 'separator' },
       {
         label: '&Trash Note',
-        visible: isAuthenticated,
         click: appCommandSender({ action: 'trashNote' }),
       },
-      ...(isAuthenticated ? [{ type: 'separator' }] : []),
+      { type: 'separator' },
       {
         label: 'Search &Notes…',
-        visible: isAuthenticated,
         click: appCommandSender({ action: 'focusSearchField' }),
         accelerator: 'CommandOrControl+Shift+S',
       },
       {
         label: '&Find in Note',
-        visible: isAuthenticated,
         click: appCommandSender({ action: 'focusSearchField' }),
         accelerator: 'CommandOrControl+F',
       },
       {
         label: 'Find A&gain',
-        visible: isAuthenticated,
         click: editorCommandSender({ action: 'findAgain' }),
         accelerator: 'CommandOrControl+G',
       },
-    ],
+    ];
+  }
+
+  const defaultSubmenuAdditions = [
+    {
+      label: '&Cut',
+      role: 'cut',
+    },
+    {
+      label: 'C&opy',
+      role: 'copy',
+    },
+    {
+      label: '&Paste',
+      role: 'paste',
+    },
+    selectAll,
+    { type: 'separator' },
+  ];
+
+  const submenu = editModeMenuOptions
+    .concat(defaultSubmenuAdditions)
+    .concat(authenticatedMenuOptions);
+
+  const fileMenu = {
+    label: '&Edit',
+    submenu,
   };
+
+  return fileMenu;
 };
 
 module.exports = buildEditMenu;
