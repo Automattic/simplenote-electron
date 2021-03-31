219599239452e5ec52e29e6ed10fc817351b4c06
Carlos Garcia <fluiddot@gmail.com>
2021-03-31 13:36:46 +0200

    Add unit tests for interactive state

--- lib/components/tag-chip/test.tsx
+++ lib/components/tag-chip/test.tsx
@@ -15,30 +15,60 @@ describe('TagChip', () => {
     expect(selectIt).toHaveBeenCalled();
   });
 
-  it('should not include the `selected` class by default', () => {
-    const chip = shallow(<TagChip tagName="spline" />);
+  describe('selected state', () => {
+    it('should not include the `selected` class by default', () => {
+      const chip = shallow(<TagChip tagName="spline" />);
 
-    expect(chip.hasClass('selected')).toBe(false);
+      expect(chip.hasClass('selected')).toBe(false);
+    });
+
+    it('should include the `selected` class when selected', () => {
+      const chip = shallow(<TagChip tagName="spline" selected />);
+
+      expect(chip.hasClass('selected')).toBe(true);
+    });
+
+    it('should toggle the `selected` class with prop changes', () => {
+      const chip = shallow(<TagChip tagName="spline" />);
+
+      expect(chip.hasClass('selected')).toBe(false);
+
+      chip.setProps({ selected: true });
+
+      expect(chip.hasClass('selected')).toBe(true);
+
+      chip.setProps({ selected: false });
+
+      expect(chip.hasClass('selected')).toBe(false);
+    });
   });
 
-  it('should include the `selected` class when selected', () => {
-    const chip = shallow(<TagChip tagName="spline" selected />);
+  describe('interactive state', () => {
+    it('should include the `interactive` class by default', () => {
+      const chip = shallow(<TagChip tagName="spline" />);
 
-    expect(chip.hasClass('selected')).toBe(true);
-  });
+      expect(chip.hasClass('interactive')).toBe(true);
+    });
 
-  it('should toggle the `selected` class with prop changes', () => {
-    const chip = shallow(<TagChip tagName="spline" />);
+    it('should not include the `interactive` class when is not interactive', () => {
+      const chip = shallow(<TagChip tagName="spline" interactive={false} />);
 
-    expect(chip.hasClass('selected')).toBe(false);
+      expect(chip.hasClass('interactive')).toBe(false);
+    });
 
-    chip.setProps({ selected: true });
+    it('should toggle the `interactive` class with prop changes', () => {
+      const chip = shallow(<TagChip tagName="spline" />);
 
-    expect(chip.hasClass('selected')).toBe(true);
+      expect(chip.hasClass('interactive')).toBe(true);
 
-    chip.setProps({ selected: false });
+      chip.setProps({ interactive: false });
 
-    expect(chip.hasClass('selected')).toBe(false);
+      expect(chip.hasClass('interactive')).toBe(false);
+
+      chip.setProps({ interactive: true });
+
+      expect(chip.hasClass('interactive')).toBe(true);
+    });
   });
 
   it('should not introduce visual regressions', () => {
