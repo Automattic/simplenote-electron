1fbe9087ba737dab0d35d0dfc307950afd3ecfba
Carlos Garcia <fluiddot@gmail.com>
2021-04-06 13:39:33 +0200

    Add deleted state to tag chip component

---
+++ lib/icons/revisions-small.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function RevisionsIconSmall() {
+  return (
+    <svg
+      className="icon-revisions-small"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <rect x="0" fill="none" width="24" height="24" />
+      <path d="M9.48 15.94 13 12.4V7H11v4.6L8.06 14.52ZM12 3a9 9 0 0 1 9 9H19a7 7 0 1 0-7 7 6.83 6.83 0 0 0 5.7-3H14V14h7v7H19V17.7A9 9 0 1 1 12 3Z" />
+    </svg>
+  );
+}
