46800769a021ce56bcbf3532f757e7883d1d6d6a
Carlos Garcia <fluiddot@gmail.com>
2021-04-08 13:51:20 +0200

    Add deleted tags toggle

--- lib/state/action-types.ts
+++ lib/state/action-types.ts
@@ -132,6 +132,7 @@ export type SystemThemeUpdate = Action<
 >;
 export type ToggleAnalytics = Action<'TOGGLE_ANALYTICS'>;
 export type ToggleAutoHideMenuBar = Action<'TOGGLE_AUTO_HIDE_MENU_BAR'>;
+export type ToggleDeletedTags = Action<'DELETED_TAGS_TOGGLE'>;
 export type ToggleEditMode = Action<'TOGGLE_EDIT_MODE'>;
 export type ToggleFocusMode = Action<'TOGGLE_FOCUS_MODE'>;
 export type ToggleKeyboardShortcuts = Action<'KEYBOARD_SHORTCUTS_TOGGLE'>;
@@ -421,6 +422,7 @@ export type ActionType =
   | TagRefresh
   | ToggleAnalytics
   | ToggleAutoHideMenuBar
+  | ToggleDeletedTags
   | ToggleEditMode
   | ToggleFocusMode
   | ToggleKeyboardShortcuts
