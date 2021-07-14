e1838802ca006facec12ced71faab3a4ae2b72f6
Sandy McFadden <sandy.mcfadden@automattic.com>
2021-07-14 12:53:50 -0300

    Build: webapp [ci skip]

--- setup-tests.js
+++
@@ -1,7 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import mockIndexedDB from 'fake-indexeddb';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-global.indexedDB = mockIndexedDB;
