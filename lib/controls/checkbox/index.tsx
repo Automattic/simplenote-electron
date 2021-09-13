7da9578a84f4e19cf6d45f9ec75bfa5bf482fd42
Sandy McFadden <sandy.mcfadden@automattic.com>
2021-09-13 09:47:49 -0300

    Move svg's to their own components

--- lib/controls/checkbox/index.tsx
+++ lib/controls/checkbox/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import CheckedCheckbox from '../../icons/checkbox-checked';
+import UncheckedCheckbox from '../../icons/checkbox-unchecked';
+
 type OwnProps = React.HTMLProps<HTMLInputElement> & {
   className?: string;
   onChange: () => any;
@@ -26,23 +29,9 @@ function CheckboxControl({
           <span className="checkbox-control-checked" />
         </span>
       ) : checked ? (
-        <svg
-          className="checked"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <rect x="0" fill="none" width="24" height="24" />
-          <path d="M19 3H5A2 2 0 0 0 3 5V19a2 2 0 0 0 2 2H19a2 2 0 0 0 2-2V5A2 2 0 0 0 19 3ZM10.14 16.85 5.76 12.48l1.42-1.42 3 3 6.63-6.63 1.41 1.42Z" />
-        </svg>
+        <CheckedCheckbox />
       ) : (
-        <svg
-          className="unchecked"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <rect x="0" fill="none" width="24" height="24" />
-          <path d="M19 5h0V19H5V5H19m0-2H5A2 2 0 0 0 3 5V19a2 2 0 0 0 2 2H19a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2Z" />
-        </svg>
+        <UncheckedCheckbox />
       )}
     </span>
   );
