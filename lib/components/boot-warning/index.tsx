eaacbfe6b0900c1f49ae55068e995ddb688505a1
Kat Hagan <kat@codebykat.com>
2024-05-14 15:05:40 -0400

    Build: webapp-develop [ci skip]

--- lib/components/boot-warning/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FunctionComponent } from 'react';
-import './style';
-
-const BootWarning: FunctionComponent = ({ children }) => (
-  <h3 className="boot-warning__message">{children}</h3>
-);
-
-export default BootWarning;
