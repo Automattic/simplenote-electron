689bbf325174b1c1ff1ca3f749b26679a3f4d694
Gio Lodi <gio@mokacoding.com>
2024-05-30 12:48:45 +1000

    Add `--force`

--- .buildkite/commands/install_node_dependencies.sh
+++ .buildkite/commands/install_node_dependencies.sh
@@ -3,4 +3,8 @@
 echo "--- :npm: Install Node dependencies"
 # --legacy-peer-deps is necessary because of react-monaco-editor.
 # See README for more details
-npm ci --legacy-peer-deps
+#
+# A note on --force: Using it as an experiment to see if the macOS build works.
+# The flag is used in CircleCI, but lint and test worked on macOS without it.
+# See https://buildkite.com/automattic/simplenote-electron/builds/13#018fc75d-ed95-4c02-aaa7-a343a0a1d38a
+npm ci --legacy-peer-deps --force
