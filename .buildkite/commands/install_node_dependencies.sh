542a7ec6ee118d696fcd776ed434f990faa981b4
Gio Lodi <gio@mokacoding.com>
2024-05-30 13:47:46 +1000

    Revert "Add `--force`"
    This reverts commit 4ebaab1626d6d21d49c98658fc495d501f9a9628.

--- .buildkite/commands/install_node_dependencies.sh
+++ .buildkite/commands/install_node_dependencies.sh
@@ -3,8 +3,4 @@
 echo "--- :npm: Install Node dependencies"
 # --legacy-peer-deps is necessary because of react-monaco-editor.
 # See README for more details
-#
-# A note on --force: Using it as an experiment to see if the macOS build works.
-# The flag is used in CircleCI, but lint and test worked on macOS without it.
-# See https://buildkite.com/automattic/simplenote-electron/builds/13#018fc75d-ed95-4c02-aaa7-a343a0a1d38a
-npm ci --legacy-peer-deps --force
+npm ci --legacy-peer-deps
