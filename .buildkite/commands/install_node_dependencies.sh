201e0f8f15aba846d7fbb6f66cdd4fa8e51c3bef
Gio Lodi <gio@mokacoding.com>
2024-05-31 18:15:39 +1000

    Revert "Call `npm ci` with `--include=optional` in Buildkite"
    This reverts commit d23b7eb5319f94a8698553675aad84590255e7c8.

--- .buildkite/commands/install_node_dependencies.sh
+++ .buildkite/commands/install_node_dependencies.sh
@@ -3,7 +3,4 @@
 echo "--- :npm: Install Node dependencies"
 # --legacy-peer-deps is necessary because of react-monaco-editor.
 # See README for more details
-#
-# --include=optional is an attempt to fix the missing dmg-license which we are seeing in Buildkite
-# See https://buildkite.com/automattic/simplenote-electron/builds/40#018fcd52-a38f-4a75-9ee3-474384ccdee7
-npm ci --legacy-peer-deps --include=optional
+npm ci --legacy-peer-deps
