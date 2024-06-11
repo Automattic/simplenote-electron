611019bc7346447d915e4b4c2191b725425f0819
Gio Lodi <gio@mokacoding.com>
2024-06-11 18:07:18 +1000

    Add steps to configure code signing for Windows
    See https://www.electron.build/code-signing#windows

--- .buildkite/commands/package_windows.ps1
+++ .buildkite/commands/package_windows.ps1
@@ -3,6 +3,27 @@ $ErrorActionPreference = "Stop"
 
 & "prepare_windows_host_for_node.ps1"
 
+Write-Host "--- :windows: Configure Windows code signing"
+# The pfx path comes from the prepare script above.
+# TODO: Move the set instruction in the script at the plugin level?
+$certPath = (Convert-Path .\certificate.pfx)
+If (Test-Path $certPath) {
+    [System.Environment]::SetEnvironmentVariable('CSC_LINK', $certPath, [System.EnvironmentVariableTarget]::Machine)
+    Write-Host "Environment variable CSC_LINK set to $certPath"
+} else {
+    Write-Host "[!] certificate.pfx file does not exist."
+    Exit 1
+}
+
+$windowsCertPassword = [System.Environment]::GetEnvironmentVariable('WINDOWS_CODE_SIGNING_CERT_PASSWORD', [System.EnvironmentVariableTarget]::Machine)
+If ([string]::IsNullOrEmpty($windowsCertPassword)) {
+    Write-Host "[!] WINDOWS_CODE_SIGNING_CERT_PASSWORD is not set in the environment."
+    Exit 1
+} else {
+    [System.Environment]::SetEnvironmentVariable('CSC_KEY_PASSWORD', $windowsCertPassword, [System.EnvironmentVariableTarget]::Machine)
+    Write-Host "Environment variable CSC_KEY_PASSWORD set to the value of WINDOWS_CODE_SIGNING_CERT_PASSWORD."
+}
+
 Write-Host "--- :windows: Installing make"
 choco install make
 
