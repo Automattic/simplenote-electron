d5090286ed94b863fd2d0405ebaa6845206a4ec1
Kat Hagan <kat@codebykat.com>
2024-06-13 03:04:07 -0400

    update some showdown options and try a naive way of fixing indented paragraphs to not be code blocks

--- lib/utils/render-note-to-html.ts
+++ lib/utils/render-note-to-html.ts
@@ -21,16 +21,28 @@ export const renderNoteToHtml = (content: string) => {
         extensions: ['enableCheckboxes', 'removeLineBreaks'],
       });
       markdownConverter.setFlavor('github');
-      markdownConverter.setOption('simpleLineBreaks', false); // override GFM
       markdownConverter.setOption('ghMentions', false);
+      markdownConverter.setOption('literalMidWordUnderscores', true);
+      markdownConverter.setOption('simpleLineBreaks', false); // override GFM
       markdownConverter.setOption('smoothLivePreview', true);
-      markdownConverter.setOption('tables', true); // table syntax
+      markdownConverter.setOption('splitAdjacentBlockquotes', true);
       markdownConverter.setOption('strikethrough', true); // ~~strikethrough~~
+      markdownConverter.setOption('tables', true); // table syntax
 
-      const transformedContent = content.replace(
+      let transformedContent = content.replace(
         /([ \t\u2000-\u200a]*)\u2022(\s)/gm,
         '$1-$2'
       ); // normalized bullets
+
+      // remove tab indentation on paragraphs
+      // TODO this is very naive and fragile and will break in a lot of cases, such as:
+      // - different size indentations
+      // - paragraphs that begin with a number or a list character
+      // - list characters other than - and *
+      transformedContent = content.replace(
+        /\n {4}(([^-*\d]))/g,
+        '\n&nbsp;&nbsp;&nbsp;$1'
+      );
 
       return sanitizeHtml(markdownConverter.makeHtml(transformedContent));
     }
