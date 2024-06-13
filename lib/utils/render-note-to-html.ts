9be99ec63f16c6e7d7b831e8ed1dc266d530efe4
Kat Hagan <kat@codebykat.com>
2024-06-13 03:29:33 -0400

    remove code to strip out linebreaks in preview

--- lib/utils/render-note-to-html.ts
+++ lib/utils/render-note-to-html.ts
@@ -12,19 +12,11 @@ const removeLineBreaks = {
   replace: '>',
 };
 
-const removeParagraphIndent = {
-  type: 'output',
-  regex: 'longtext',
-  replace: 'REMOVED',
-};
-
 export const renderNoteToHtml = (content: string) => {
   return import(/* webpackChunkName: 'showdown' */ 'showdown').then(
     ({ default: showdown }) => {
-      console.log(content);
       showdown.extension('enableCheckboxes', enableCheckboxes);
       showdown.extension('removeLineBreaks', removeLineBreaks);
-      showdown.extension('removeParagraphIndent', removeParagraphIndent);
       const markdownConverter = new showdown.Converter({
         extensions: ['enableCheckboxes', 'removeLineBreaks'],
       });
@@ -37,20 +29,10 @@ export const renderNoteToHtml = (content: string) => {
       markdownConverter.setOption('strikethrough', true); // ~~strikethrough~~
       markdownConverter.setOption('tables', true); // table syntax
 
-      let transformedContent = content.replace(
+      const transformedContent = content.replace(
         /([ \t\u2000-\u200a]*)\u2022(\s)/gm,
         '$1-$2'
       ); // normalized bullets
-
-      // remove tab indentation on paragraphs
-      // TODO this is very naive and fragile and will break in a lot of cases, such as:
-      // - different size indentations
-      // - paragraphs that begin with a number or a list character
-      // - list characters other than - and *
-      transformedContent = content.replace(
-        /\n {4}(([^-*\d]))/g,
-        '\n&nbsp;&nbsp;&nbsp;$1'
-      );
 
       return sanitizeHtml(markdownConverter.makeHtml(transformedContent));
     }
