eb239cfaaf0a1f132e47bfbba5bdbbd0b58d9802
Kat Hagan <kat@codebykat.com>
2024-07-11 14:26:59 -0400

    Revert "Don't pass a variable to require, it makes webpack mad"
    This reverts commit e65a4f7b75653187060b767955a5ec5e2fd043ea.

--- get-config.js
+++ get-config.js
@@ -1,6 +1,7 @@
 function readConfig() {
+  const configPath = './config-local';
   try {
-    const config = require('./config-local');
+    const config = require(configPath);
     if (typeof config === 'function') {
       throw new Error('Invalid config file. Config must be JSON.');
     }
@@ -8,7 +9,7 @@ function readConfig() {
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(
-      `Could not load the required configuration file.\n` +
+      `Could not load the required configuration file at ${configPath}.\n` +
         'Please consult the project README.md for further information.'
     );
 
