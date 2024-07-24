b98776269922efd7f415162df791bb3fc89a33ce
Gio Lodi <gio@mokacoding.com>
2024-07-24 14:05:51 +1000

    Add a Buildkite header to step to decrypt cert

--- .buildkite/commands/decrypt-dedicated-windows-cert.sh
+++ .buildkite/commands/decrypt-dedicated-windows-cert.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -eu
 
+echo "--- :lock_with_ink_pen: Decrypt Simplenote Windows code signing certificate"
+
 openssl aes-256-cbc -md md5 -d \
   -in ./resources/certificates/win.p12.enc \
   -out ./resources/certificates/win.p12 \
   -k "$DEDICATED_WINDOWS_CERT_ENCRYPTION_KEY"
-
