35dd976fe1bc7e3c36725c8d9474b49b9b0d2a34
Usiel Riedl <usiel.riedl@automattic.com>
2026-06-18 17:32:45 +0800

    Use tabler icons as placeholders for table

--- lib/markdown-editor/toolbar-icons.tsx
+++ lib/markdown-editor/toolbar-icons.tsx
@@ -8,6 +8,25 @@ type IconProps = { className?: string };
 // SVG's intrinsic 24×24 viewBox; the rendered size is driven by CSS.
 const ICON_SIZE = 24;
 
+const makeTablerStrokeIcon = (paths: string[], displayName: string) => {
+  const Icon = ({ className }: IconProps) => (
+    <svg
+      className={['toolbar-icon-stroke', className].filter(Boolean).join(' ')}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={ICON_SIZE}
+      height={ICON_SIZE}
+      aria-hidden
+    >
+      {paths.map((d) => (
+        <path key={d} d={d} />
+      ))}
+    </svg>
+  );
+  Icon.displayName = displayName;
+  return Icon;
+};
+
 const makeIcon = (icon: string) => {
   const Icon = ({ className }: IconProps) => (
     <Gridicon icon={icon} size={ICON_SIZE} className={className} />
@@ -35,133 +54,68 @@ export const HeadingIcon = ({
   <Gridicon icon={`heading-h${level}`} size={ICON_SIZE} className={className} />
 );
 
-// TODO: Find prettier table icons
-export const TableIcon = ({ className }: IconProps) => (
-  <svg
-    className={['toolbar-icon-stroke', className].filter(Boolean).join(' ')}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width={ICON_SIZE}
-    height={ICON_SIZE}
-    aria-hidden
-  >
-    <path d="M5 6h14a1.5 1.5 0 0 1 1.5 1.5v9A1.5 1.5 0 0 1 19 18H5a1.5 1.5 0 0 1-1.5-1.5v-9A1.5 1.5 0 0 1 5 6" />
-    <path d="M5 10.5h14" />
-    <path d="M5 14.5h14" />
-    <path d="M11.5 6v12" />
-  </svg>
+// Tabler Icons (MIT): https://tabler.io/icons
+export const TableIcon = makeTablerStrokeIcon(
+  [
+    'M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14',
+    'M3 10h18',
+    'M10 3v18',
+  ],
+  'TableIcon'
 );
 
-export const InsertTableRowAboveIcon = ({ className }: IconProps) => (
-  <svg
-    className={['toolbar-icon-stroke', className].filter(Boolean).join(' ')}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width={ICON_SIZE}
-    height={ICON_SIZE}
-    aria-hidden
-  >
-    <path d="M5 14h14a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1" />
-    <path d="M12 4v4" />
-    <path d="M10 6h4" />
-  </svg>
+export const InsertTableRowAboveIcon = makeTablerStrokeIcon(
+  [
+    'M4 18v-4a1 1 0 0 1 1 -1h14a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-14a1 1 0 0 1 -1 -1',
+    'M12 9v-4',
+    'M10 7h4',
+  ],
+  'InsertTableRowAboveIcon'
 );
 
-export const InsertTableRowBelowIcon = ({ className }: IconProps) => (
-  <svg
-    className={['toolbar-icon-stroke', className].filter(Boolean).join(' ')}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width={ICON_SIZE}
-    height={ICON_SIZE}
-    aria-hidden
-  >
-    <path d="M5 6h14a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1" />
-    <path d="M12 16v4" />
-    <path d="M10 18h4" />
-  </svg>
+export const InsertTableRowBelowIcon = makeTablerStrokeIcon(
+  [
+    'M20 6v4a1 1 0 0 1 -1 1h-14a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h14a1 1 0 0 1 1 1',
+    'M12 15v4',
+    'M14 17h-4',
+  ],
+  'InsertTableRowBelowIcon'
 );
 
-export const DeleteTableRowIcon = ({ className }: IconProps) => (
-  <svg
-    className={['toolbar-icon-stroke', className].filter(Boolean).join(' ')}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width={ICON_SIZE}
-    height={ICON_SIZE}
-    aria-hidden
-  >
-    <path
-      className="toolbar-icon-delete-shape"
-      d="M3 8h4M17 8h4M3 16h4M17 16h4M3 8v8M21 8v8"
-    />
-    <rect
-      className="toolbar-icon-delete-ring"
-      height="10"
-      rx="2"
-      width="10"
-      x="7"
-      y="7"
-    />
-    <path className="toolbar-icon-delete-mark" d="M9.75 9.75 14.25 14.25" />
-    <path className="toolbar-icon-delete-mark" d="M14.25 9.75 9.75 14.25" />
-  </svg>
+export const DeleteTableRowIcon = makeTablerStrokeIcon(
+  [
+    'M20 6v4a1 1 0 0 1 -1 1h-14a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h14a1 1 0 0 1 1 1',
+    'M10 16l4 4',
+    'M10 20l4 -4',
+  ],
+  'DeleteTableRowIcon'
 );
 
-export const InsertTableColumnBeforeIcon = ({ className }: IconProps) => (
-  <svg
-    className={['toolbar-icon-stroke', className].filter(Boolean).join(' ')}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width={ICON_SIZE}
-    height={ICON_SIZE}
-    aria-hidden
-  >
-    <path d="M14 5h5a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1" />
-    <path d="M4 12h4" />
-    <path d="M6 10v4" />
-  </svg>
+export const InsertTableColumnBeforeIcon = makeTablerStrokeIcon(
+  [
+    'M14 4h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1',
+    'M5 12h4',
+    'M7 10v4',
+  ],
+  'InsertTableColumnBeforeIcon'
 );
 
-export const InsertTableColumnAfterIcon = ({ className }: IconProps) => (
-  <svg
-    className={['toolbar-icon-stroke', className].filter(Boolean).join(' ')}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width={ICON_SIZE}
-    height={ICON_SIZE}
-    aria-hidden
-  >
-    <path d="M5 5h5a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1" />
-    <path d="M16 12h4" />
-    <path d="M18 10v4" />
-  </svg>
+export const InsertTableColumnAfterIcon = makeTablerStrokeIcon(
+  [
+    'M6 4h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1',
+    'M15 12h4',
+    'M17 10v4',
+  ],
+  'InsertTableColumnAfterIcon'
 );
 
-export const DeleteTableColumnIcon = ({ className }: IconProps) => (
-  <svg
-    className={['toolbar-icon-stroke', className].filter(Boolean).join(' ')}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width={ICON_SIZE}
-    height={ICON_SIZE}
-    aria-hidden
-  >
-    <path
-      className="toolbar-icon-delete-shape"
-      d="M8 3v4M8 17v4M16 3v4M16 17v4M8 3h8M8 21h8"
-    />
-    <rect
-      className="toolbar-icon-delete-ring"
-      height="10"
-      rx="2"
-      width="10"
-      x="7"
-      y="7"
-    />
-    <path className="toolbar-icon-delete-mark" d="M9.75 9.75 14.25 14.25" />
-    <path className="toolbar-icon-delete-mark" d="M14.25 9.75 9.75 14.25" />
-  </svg>
+export const DeleteTableColumnIcon = makeTablerStrokeIcon(
+  [
+    'M6 4h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1',
+    'M16 10l4 4',
+    'M16 14l4 -4',
+  ],
+  'DeleteTableColumnIcon'
 );
 
 // Gridicons has no dedicated code-block glyph, so we draw one in the same visual
